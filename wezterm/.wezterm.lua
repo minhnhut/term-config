e5fc9ee15621b1ef31fa857ef1d8c7496bbd0be6
Trần Minh Nhựt <minhnhut@users.noreply.github.com>
2026-01-12 22:37:09 +0700

    Make wezterm behave like tmux

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,24 +1,117 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-
--- For example, changing the initial geometry for new windows:
+-- Window settings
 config.initial_cols = 120
 config.initial_rows = 28
-
--- or, changing the font size and color scheme.
-config.font_size = 15
-config.font = wezterm.font("BlexMono Nerd Font Mono")
-config.color_scheme = "Dracula"
 config.hide_tab_bar_if_only_one_tab = true
 
--- Finally, return the configuration to wezterm:
+-- Font settings
+config.font = wezterm.font("BlexMono Nerd Font Mono")
+config.font_size = 15
+
+-- Color scheme
+config.color_scheme = "Dracula"
+
+-- Leader key
+config.leader = { key = "a", mods = "CTRL" }
+
+-- Tab bar styling (like tmux status bar at top)
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = false
+config.tab_max_width = 32
+
+-- Dracula-inspired colors for tab bar
+config.colors = {
+  tab_bar = {
+    background = "#282a36",
+    active_tab = {
+      bg_color = "#50fa7b",
+      fg_color = "#282a36",
+      intensity = "Bold",
+    },
+    inactive_tab = {
+      bg_color = "#1d1d21",
+      fg_color = "#f8f8f2",
+    },
+    inactive_tab_hover = {
+      bg_color = "#6272a4",
+      fg_color = "#f8f8f2",
+    },
+    new_tab = {
+      bg_color = "#6272a4",
+      fg_color = "#f8f8f2",
+    },
+  },
+}
+
+-- Keybindings (adapted from your tmux.conf)
 config.keys = {
-  {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
+  -- Send C-a when pressing leader twice
+  { key = "a", mods = "LEADER|CTRL", action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }) },
+
+  -- Pane navigation (C-hjkl without leader, like your tmux)
+  { key = "h", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Left") },
+  { key = "j", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Down") },
+  { key = "k", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Up") },
+  { key = "l", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Right") },
+
+  -- Tab navigation (C-q previous, C-e next, C-Tab last)
+  { key = "q", mods = "CTRL", action = wezterm.action.ActivateTabRelative(-1) },
+  { key = "e", mods = "CTRL", action = wezterm.action.ActivateTabRelative(1) },
+  { key = "Tab", mods = "CTRL", action = wezterm.action.ActivateLastTab },
+
+  -- Create new tab with leader+c (opens in current dir)
+  { key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+
+  -- Split panes (leader+" for vertical, leader+% for horizontal)
+  { key = '"', mods = "LEADER|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "%", mods = "LEADER|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+  -- Additional splits
+  { key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "\\", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "s", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "v", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+  -- Zoom pane
+  { key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+
+  -- Reload config (leader+r like tmux)
+  { key = "r", mods = "LEADER", action = wezterm.action.ReloadConfiguration },
+
+  -- Copy mode (leader+[ like tmux)
+  { key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+
+  -- Close pane
+  { key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+
+  -- Resize panes (leader+hjkl with shift)
+  { key = "H", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
+  { key = "J", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
+  { key = "K", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+  { key = "L", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
+
+  -- Tab selection (leader+1-9)
+  { key = "1", mods = "LEADER", action = wezterm.action.ActivateTab(0) },
+  { key = "2", mods = "LEADER", action = wezterm.action.ActivateTab(1) },
+  { key = "3", mods = "LEADER", action = wezterm.action.ActivateTab(2) },
+  { key = "4", mods = "LEADER", action = wezterm.action.ActivateTab(3) },
+  { key = "5", mods = "LEADER", action = wezterm.action.ActivateTab(4) },
+  { key = "6", mods = "LEADER", action = wezterm.action.ActivateTab(5) },
+  { key = "7", mods = "LEADER", action = wezterm.action.ActivateTab(6) },
+  { key = "8", mods = "LEADER", action = wezterm.action.ActivateTab(7) },
+  { key = "9", mods = "LEADER", action = wezterm.action.ActivateTab(8) },
+
+}
+
+-- Copy mode with vi keys
+config.key_tables = {
+  copy_mode = {
+    { key = "y", mods = "NONE", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
+    { key = "Escape", mods = "NONE", action = wezterm.action.CopyMode("Close") },
+  },
 }
 
 return config
