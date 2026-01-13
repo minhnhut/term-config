39ca5784b77cd3172b3cc0fb94e99763a2f17167
Nhut Tran <nhut.tran@tmnsolutions.vn>
2026-01-13 09:20:46 +0700

    Try using wezterm as a real tmux, improve nvim keybinding

--- nvim/init.lua
+++ nvim/init.lua
@@ -840,31 +840,13 @@ require('lazy').setup({
     --- @type blink.cmp.Config
     opts = {
       keymap = {
-        -- 'default' (recommended) for mappings similar to built-in completions
-        --   <c-y> to accept ([y]es) the completion.
-        --    This will auto-import if your LSP supports it.
-        --    This will expand snippets if the LSP sent a snippet.
-        -- 'super-tab' for tab to accept
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- For an understanding of why the 'default' preset is recommended,
-        -- you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        --
-        -- All presets have the following mappings:
-        -- <tab>/<s-tab>: move to right/left of your snippet expansion
-        -- <c-space>: Open menu or open docs if already open
-        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-        -- <c-e>: Hide menu
-        -- <c-k>: Toggle signature help
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
-
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        -- Tab to cycle through completions, Enter to accept
+        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<Esc>'] = { 'hide', 'fallback' },
+        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
       },
 
       appearance = {
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 -- Window settings
 config.initial_cols = 120
@@ -24,94 +24,106 @@ config.tab_max_width = 32
 
 -- Dracula-inspired colors for tab bar
 config.colors = {
-  tab_bar = {
-    background = "#282a36",
-    active_tab = {
-      bg_color = "#50fa7b",
-      fg_color = "#282a36",
-      intensity = "Bold",
-    },
-    inactive_tab = {
-      bg_color = "#1d1d21",
-      fg_color = "#f8f8f2",
-    },
-    inactive_tab_hover = {
-      bg_color = "#6272a4",
-      fg_color = "#f8f8f2",
-    },
-    new_tab = {
-      bg_color = "#6272a4",
-      fg_color = "#f8f8f2",
-    },
-  },
+	tab_bar = {
+		background = "#282a36",
+		active_tab = {
+			bg_color = "#50fa7b",
+			fg_color = "#282a36",
+			intensity = "Bold",
+		},
+		inactive_tab = {
+			bg_color = "#1d1d21",
+			fg_color = "#f8f8f2",
+		},
+		inactive_tab_hover = {
+			bg_color = "#6272a4",
+			fg_color = "#f8f8f2",
+		},
+		new_tab = {
+			bg_color = "#6272a4",
+			fg_color = "#f8f8f2",
+		},
+	},
 }
 
 -- Keybindings (adapted from your tmux.conf)
 config.keys = {
-  -- Send C-a when pressing leader twice
-  { key = "a", mods = "LEADER|CTRL", action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }) },
+	-- Send C-a when pressing leader twice
+	{ key = "a", mods = "LEADER|CTRL", action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }) },
 
-  -- Pane navigation (C-hjkl without leader, like your tmux)
-  { key = "h", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Left") },
-  { key = "j", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Down") },
-  { key = "k", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Up") },
-  { key = "l", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Right") },
+	-- Pane navigation (C-hjkl without leader, like your tmux)
+	{ key = "h", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Right") },
 
-  -- Tab navigation (C-q previous, C-e next, C-Tab last)
-  { key = "q", mods = "CTRL", action = wezterm.action.ActivateTabRelative(-1) },
-  { key = "e", mods = "CTRL", action = wezterm.action.ActivateTabRelative(1) },
-  { key = "Tab", mods = "CTRL", action = wezterm.action.ActivateLastTab },
+	-- Tab navigation (C-q previous, C-e next, C-Tab last)
+	{ key = "q", mods = "CTRL", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "e", mods = "CTRL", action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "Tab", mods = "CTRL", action = wezterm.action.ActivateLastTab },
 
-  -- Create new tab with leader+c (opens in current dir)
-  { key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	-- Create new tab with leader+c (opens in current dir)
+	{ key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
 
-  -- Split panes (leader+" for vertical, leader+% for horizontal)
-  { key = '"', mods = "LEADER|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-  { key = "%", mods = "LEADER|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	-- Split panes (leader+" for vertical, leader+% for horizontal)
+	{ key = '"', mods = "LEADER|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "%", mods = "LEADER|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 
-  -- Additional splits
-  { key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-  { key = "\\", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-  { key = "s", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-  { key = "v", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	-- Additional splits
+	{ key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "\\", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "s", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "v", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 
-  -- Zoom pane
-  { key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+	-- Zoom pane
+	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
 
-  -- Reload config (leader+r like tmux)
-  { key = "r", mods = "LEADER", action = wezterm.action.ReloadConfiguration },
+	-- Reload config (leader+r like tmux)
+	{ key = "r", mods = "LEADER", action = wezterm.action.ReloadConfiguration },
 
-  -- Copy mode (leader+[ like tmux)
-  { key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+	-- Copy mode (leader+[ like tmux)
+	{ key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
 
-  -- Close pane
-  { key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+	-- Close pane
+	{ key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
 
-  -- Resize panes (leader+hjkl with shift)
-  { key = "H", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
-  { key = "J", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
-  { key = "K", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
-  { key = "L", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
+	-- Resize panes (leader+hjkl with shift)
+	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
+	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
+	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
 
-  -- Tab selection (leader+1-9)
-  { key = "1", mods = "LEADER", action = wezterm.action.ActivateTab(0) },
-  { key = "2", mods = "LEADER", action = wezterm.action.ActivateTab(1) },
-  { key = "3", mods = "LEADER", action = wezterm.action.ActivateTab(2) },
-  { key = "4", mods = "LEADER", action = wezterm.action.ActivateTab(3) },
-  { key = "5", mods = "LEADER", action = wezterm.action.ActivateTab(4) },
-  { key = "6", mods = "LEADER", action = wezterm.action.ActivateTab(5) },
-  { key = "7", mods = "LEADER", action = wezterm.action.ActivateTab(6) },
-  { key = "8", mods = "LEADER", action = wezterm.action.ActivateTab(7) },
-  { key = "9", mods = "LEADER", action = wezterm.action.ActivateTab(8) },
+	-- Tab selection (leader+1-9)
+	{ key = "1", mods = "LEADER", action = wezterm.action.ActivateTab(0) },
+	{ key = "2", mods = "LEADER", action = wezterm.action.ActivateTab(1) },
+	{ key = "3", mods = "LEADER", action = wezterm.action.ActivateTab(2) },
+	{ key = "4", mods = "LEADER", action = wezterm.action.ActivateTab(3) },
+	{ key = "5", mods = "LEADER", action = wezterm.action.ActivateTab(4) },
+	{ key = "6", mods = "LEADER", action = wezterm.action.ActivateTab(5) },
+	{ key = "7", mods = "LEADER", action = wezterm.action.ActivateTab(6) },
+	{ key = "8", mods = "LEADER", action = wezterm.action.ActivateTab(7) },
+	{ key = "9", mods = "LEADER", action = wezterm.action.ActivateTab(8) },
 
+	{
+		key = ",",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 
 -- Copy mode with vi keys
 config.key_tables = {
-  copy_mode = {
-    { key = "y", mods = "NONE", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
-    { key = "Escape", mods = "NONE", action = wezterm.action.CopyMode("Close") },
-  },
+	copy_mode = {
+		{ key = "y", mods = "NONE", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
+		{ key = "Escape", mods = "NONE", action = wezterm.action.CopyMode("Close") },
+	},
 }
 
 return config
